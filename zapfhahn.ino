31a7ec5a76037324e1795d3ada8491eb04454947
Max Schipper <max.schipper1@gmx.de>
2024-12-10 14:53:04 +0100

    oberes_ventil_open(int time) methode hinzugefügt

--- zapfhahn.ino
+++ zapfhahn.ino
@@ -1,10 +1,12 @@
-#define O_OPEN_TIME 1000
-#define U_OPEN_TIME 1000
-
-
 const int buttonPin = 2;   // GPIO Taster
 const int relayPin1 = 8;    // GPIO Relais 1 für oberes Ventil
 const int relayPin2 = 9;    // GPIO Relais 2 für unteres Ventil
+
+const int obere_open_time = 1000;
+const int untere_open_time = 1000;
+// #define O_OPEN_TIME 1000
+// #define U_OPEN_TIME 1000
+
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP); // Pullup um Störsignale (float) zu verhindern
   pinMode(relayPin1, OUTPUT);        
@@ -12,17 +14,28 @@ void setup() {
   digitalWrite(relayPin, LOW);      
 }
 
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+void oberes_ventil_open(int open_time) {
+
+    digitalWrite(relayPin1, HIGH); // Relais einschalten
+    delay(open_time); // wie lange das obere Ventil geöffnet sein soll 
+    digitalWrite(relayPin1, LOW); // Relais ausschalten
+}
+
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
 void loop() {
   // Knopfstatus lesen (LOW = gedrückt, HIGH = nicht gedrückt)
   int buttonState = digitalRead(buttonPin);
 
   if (buttonState == LOW) {
-
-// in methode auslagern
-    digitalWrite(relayPin1, HIGH); // Relais einschalten
-    delay(O_OPEN_TIME); // wie lange das obere Ventil geöffnet sein soll 
-    digitalWrite(relayPin1, LOW); // Relais ausschalten
-// bis hier hin
+    oberes_ventil_open(obere_open_time);
 
 
   } else {
