9c266d1c19bc933548c420c3e5be520f2db98634
Max Schipper <max.schipper1@gmx.de>
2024-12-10 19:13:41 +0100

    added enum State and started adding serial output

--- zapfhahn.ino
+++ zapfhahn.ino
@@ -2,19 +2,42 @@ const int buttonPin = 2;   // GPIO Taster
 const int relayPin1 = 8;    // GPIO Relais 1 für oberes Ventil
 const int relayPin2 = 9;    // GPIO Relais 2 für unteres Ventil
 
-const int obere_open_time = 1000;
-const int untere_open_time = 1000;
-// #define O_OPEN_TIME 1000
-// #define U_OPEN_TIME 1000
+const int open_time1 = 1000;
+const int open_time2 = 1000;
+
+bool isFilling = false;       // Zustand der Zapfanlage
+unsigned long startTime = 0;  // Zeitstempel für Ventilsteuerung
+int currentStep = 0;          // Schritt des Prozesses
+
+enum State {
+  STATE_IDLE,
+  STATE_FILLING,
+  STATE_DISPENSING
+};
+
+State currentState = STATE_IDLE;
+unsigned long startTime = 0;
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+void closeAllValves() {
+  digitalWrite(relayPin1, LOW);
+  digitalWrite(relayPin2, LOW);
+}
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP); // Pullup um Störsignale (float) zu verhindern
   pinMode(relayPin1, OUTPUT);        
   pinMode(relayPin2, OUTPUT);        
-  digitalWrite(relayPin, LOW);      
+
+  closeAllValves(); // Zu Beginn alles schließen
+  Serial.begin(9600);
 }
 
 /////////////////////////////////////////////////////////////////////////////
+
 
 
 
