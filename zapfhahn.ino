e47cc79a53585a78e72fcba00d894f0d7a98b1ab
Max Schipper <max.schipper1@gmx.de>
2024-12-10 20:18:04 +0100

    completed basic valve logic using switch cases with enum State

--- zapfhahn.ino
+++ zapfhahn.ino
@@ -2,12 +2,8 @@ const int buttonPin = 2;   // GPIO Taster
 const int relayPin1 = 8;    // GPIO Relais 1 für oberes Ventil
 const int relayPin2 = 9;    // GPIO Relais 2 für unteres Ventil
 
-const int open_time1 = 1000;
-const int open_time2 = 1000;
-
-bool isFilling = false;       // Zustand der Zapfanlage
-unsigned long startTime = 0;  // Zeitstempel für Ventilsteuerung
-int currentStep = 0;          // Schritt des Prozesses
+const int upper_open_time = 1000;
+const int lower_open_time = 1000;
 
 enum State {
   STATE_IDLE,
@@ -22,33 +18,39 @@ unsigned long startTime = 0;
 
 /////////////////////////////////////////////////////////////////////////////
 
-void closeAllValves() {
+void close_all_valves() {
   digitalWrite(relayPin1, LOW);
   digitalWrite(relayPin2, LOW);
 }
+
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP); // Pullup um Störsignale (float) zu verhindern
   pinMode(relayPin1, OUTPUT);        
   pinMode(relayPin2, OUTPUT);        
 
-  closeAllValves(); // Zu Beginn alles schließen
+  close_all_valves(); // Zu Beginn alles schließen
   Serial.begin(9600);
 }
 
 /////////////////////////////////////////////////////////////////////////////
 
 
-
-
-void oberes_ventil_open(int open_time) {
-
-    digitalWrite(relayPin1, HIGH); // Relais einschalten
-    delay(open_time); // wie lange das obere Ventil geöffnet sein soll 
-    digitalWrite(relayPin1, LOW); // Relais ausschalten
+void open_upper_valve() {
+  digitalWrite(relayPin1, HIGH);
 }
 
+void open_lower_valve() {
+  digitalWrite(relayPin2, HIGH);
+}
 
+void close_upper_valve() {
+  digitalWrite(relayPin1, LOW);
+}
+
+void close_lower_valve() {
+  digitalWrite(relayPin2, LOW);
+}
 
 
 /////////////////////////////////////////////////////////////////////////////
@@ -56,13 +58,58 @@ void oberes_ventil_open(int open_time) {
 void loop() {
   // Knopfstatus lesen (LOW = gedrückt, HIGH = nicht gedrückt)
   int buttonState = digitalRead(buttonPin);
+  bool buttonPressed = (buttonState == LOW);
 
-  if (buttonState == LOW) {
-    oberes_ventil_open(obere_open_time);
+  switch (currentState) {
+    case STATE_IDLE:
+      // Im Leerlauf warten wir auf einen Knopfdruck
+      if (buttonPressed) {
+        Serial.println("Starte Füllvorgang...");
+        open_upper_valve();
+        startTime = millis();
+        currentState = STATE_FILLING;
+      }
+      break;
 
+    case STATE_FILLING:
+      // Während des Füllens prüfen wir, ob die Füllzeit abgelaufen ist
+      // oder ob der Knopf erneut gedrückt wurde für Abbruch
+      if (buttonPressed) {
+        Serial.println("Abbruch während des Füllens!");
+        close_all_valves();
+        currentState = STATE_IDLE;
 
-  } else {
-    digitalWrite(relayPin, LOW);  
+      } else {
+        unsigned long elapsed = millis() - startTime;
+
+        if (elapsed >= upper_open_time) {
+          close_upper_valve();
+          Serial.println("Wechsle zum Entleeren...(in 1s)");
+          delay(1000);
+          open_lower_valve();
+          startTime = millis();
+          currentState = STATE_DISPENSING;
+        }
+      }
+      break;
+
+    case STATE_DISPENSING:
+      // Während des Entleerens prüfen wir ebenfalls auf Abbruch oder Ablauf der Zeit
+      if (buttonPressed) {
+        Serial.println("Abbruch während des Entleerens!");
+        close_all_valves();
+        currentState = STATE_IDLE;
+
+      } else {
+        unsigned long elapsed = millis() - startTime;
+
+        if (elapsed >= lower_open_time) {
+          close_lower_valve();
+          Serial.println("Vorgang abgeschlossen, zurück zu IDLE");
+          currentState = STATE_IDLE;
+        }
+      }
+      break;
   }
 }
 
